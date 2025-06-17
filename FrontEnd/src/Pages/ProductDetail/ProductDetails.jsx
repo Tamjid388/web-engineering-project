a7973090a97b69bf1989d939d34d2db4567c8dc4
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-06-17 14:06:27 +0600

    product Details page

--- FrontEnd/src/Pages/ProductDetail/ProductDetails.jsx
+++ FrontEnd/src/Pages/ProductDetail/ProductDetails.jsx
@@ -6,6 +6,7 @@ export const ProductDetails = () => {
     console.log(id);
   return (
     <div className="my-16 text-center space-y-2">
+        
         <h1 className="text-4xl font-bold ">ProductDetails {id}</h1>
            <p className="text-lg text-red-500 font-medium">
           ⚠️ Product detail feature will be implemented after backend integration.
