87f9cbe317de907a59be3d829a965f9054d00c10
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-06-17 13:32:12 +0600

    initialized producDetails-Page and Route

--- FrontEnd/src/Pages/ProductDetail/ProductDetails.jsx
+++ FrontEnd/src/Pages/ProductDetail/ProductDetails.jsx
@@ -1,7 +1,15 @@
+import { useParams } from "react-router"
 
 
 export const ProductDetails = () => {
+    const {id}=useParams()
+    console.log(id);
   return (
-    <div>ProductDetails</div>
+    <div className="my-16 text-center space-y-2">
+        <h1 className="text-4xl font-bold ">ProductDetails {id}</h1>
+           <p className="text-lg text-red-500 font-medium">
+          ⚠️ Product detail feature will be implemented after backend integration.
+        </p>
+    </div>
   )
 }
