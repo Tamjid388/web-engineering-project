2654057e4be5cf3b2aa671484a6295f6c46053ca
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-07-07 23:01:10 +0600

    about us

--- FrontEnd/src/Components/ProductDetail/Reviews.jsx
+++ FrontEnd/src/Components/ProductDetail/Reviews.jsx
@@ -7,15 +7,25 @@ export const Reviews = ({product}) => {
   return (
     <div>
         <h1 className='text-2xl text-start font-bold'>Reviews</h1>
-         <div className='flex flex-col md:flex-row justify-between items-center'>
+         <div className='flex flex-col gap-2
+           md:flex-row md:justify-between md:items-center'>
             <p className='text-start'>Get specific details about this product from customers who own it.</p>
             <button
             onClick={()=>setOpen(!open)}
             className='btn btn-outline btn-primary'>Write A Review</button>
          </div>
          <div className='w-full my-4'>
-           <textarea 
-           className="textarea w-full" placeholder="Write Your throughts"></textarea>
+{
+    open && <div className='flex flex-col justify-start'>
+     <textarea 
+           className="textarea w-full" placeholder="Write Your throughts">
+
+           </textarea>
+           <button className='btn btn-primary text-start w-fit my-3' type="button">Submit</button>
+    </div>
+           
+}
+
          </div>
 
         <div>
