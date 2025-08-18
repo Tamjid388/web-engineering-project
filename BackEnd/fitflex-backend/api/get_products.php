5aa6b9d4954fa7c2ba706a2ab2bb71a0955ff78d
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-08-18 19:49:13 +0600

    a

--- BackEnd/fitflex-backend/api/get_products.php
+++ BackEnd/fitflex-backend/api/get_products.php
@@ -1,5 +1,25 @@
 <?php
+
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+header("Access-Control-Allow-Credentials: true");
+
+// Handle preflight requests
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+
+
+
 include("../db.php");
+
+
+
+
 header("Content-Type: application/json");
 
 $products = [];
